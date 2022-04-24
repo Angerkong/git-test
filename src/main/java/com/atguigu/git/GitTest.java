d425a16105761c950aa4bfbd48a7bfb045dba099
hsk <2535382542@qq.com>
2022-04-24 22:21:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * Author: hsk
+ * Date: 2022/4/24 22:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
